2c1c68cfc4c9c53660c6b86dec67514aab7aaa45
Tyree Christensen <tyree83@gmail.com>
2021-03-08 17:27:01 -0500

    header and footer complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
+import { Container } from 'react-bootstrap';
 
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <>
-     <h1>Welcome to Sharry's Socks</h1> 
+    <Header />
+    <main className='py-3'>
+      <Container>
+        <h1>Welcome to Sharry's Socks</h1> 
+      </Container>
+    </main>
+    <Footer />
     </>
   );
 }

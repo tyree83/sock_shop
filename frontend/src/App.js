d4ec0eecdbb1981ae75aa83e2c7724b291b40fa3
Tyree Christensen <tyree83@gmail.com>
2021-03-22 11:10:22 -0400

    user register reducer, action

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
-import HomeScreen from './screens/HomeScreen'
-import ProductScreen from './screens/ProductScreen'
-import CartScreen from './screens/CartScreen'
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
 
 const App = () => {
   return (
@@ -14,7 +15,8 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-          <Route path='/login/' component={LoginScreen} />
+          <Route path='/login' component={LoginScreen} />
+          <Route path='/register' component={RegisterScreen} />
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
           <Route path='/' component={HomeScreen} exact />
@@ -23,6 +25,6 @@ const App = () => {
       <Footer />
     </Router>
   );
-}
+};
 
 export default App;
